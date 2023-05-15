c7da0437f47767993581866fd57be20046700842
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2023-05-16 00:55:45 +0100

    done

--- src/pages/navigation/navigation.component.jsx
+++ src/pages/navigation/navigation.component.jsx
@@ -1,7 +1,28 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/navbar/navbar.component'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { customGetCategoryAndDocumentFromCollection } from '../../utils/firebase/firebase.component'
+import { setCategories } from '../../store/categories/category.action'
 
 const Navigation = () => {
+  const [items, setItems] = useState()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const getRes = async () => {
+      const data = await customGetCategoryAndDocumentFromCollection();
+
+      // console.log(data);
+
+      dispatch(setCategories(data));
+
+      setItems(data)
+    };
+
+    return getRes;
+  }, []);
+
+  useEffect(() => { }, [items])
   return (
       <div>
       <Navbar />
