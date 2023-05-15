6b1b6810d78f07f491ee73e917ba09c936d47536
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2023-05-16 00:25:42 +0100

    cateories data fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,18 @@ import Home from "./pages/home/home.component";
 import { useEffect } from "react";
 import {
   createUserDocumentFromAuth,
-  customGetCategoryAndDocumentFromCollection,
   customOnAUthStateChange,
 } from "./utils/firebase/firebase.component";
 import { useDispatch } from "react-redux";
 import { setCurrentUSer } from "./store/user/user.action";
 import Shop from "./pages/shop/shop.component";
-import { setCategories } from "./store/categories/category.action";
 import Checkout from "./pages/checkout/checkout.comppnent";
 
 function App() {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const getRes = async () => {
-      const data = await customGetCategoryAndDocumentFromCollection();
 
-      // console.log(data);
 
-      dispatch(setCategories(data));
-    };
-
-    return getRes;
-  }, []);
-  
   useEffect(() => {
     const unsubscribe = customOnAUthStateChange((user) => {
       console.log(user);
@@ -36,16 +24,6 @@ function App() {
       createUserDocumentFromAuth(user, { uid: user.uid });
 
       dispatch(setCurrentUSer(user));
-
-      // const getRes = async () => {
-      //   const data = await customGetCategoryAndDocumentFromCollection();
-  
-      //   // console.log(data);
-  
-      //   dispatch(setCategories(data));
-      // };
-
-      // return getRes;
 
     });
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import ShoppingComponent from "../../components/shopping-component/shopping-component";
 import Sections from "../../components/sections/section.component";
+import { customGetCategoryAndDocumentFromCollection } from "../../utils/firebase/firebase.component";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setCategories } from "../../store/categories/category.action";
 
 
 const Shop = () => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const getRes = async () => {
+      const data = await customGetCategoryAndDocumentFromCollection();
+
+      // console.log(data);
+
+      dispatch(setCategories(data));
+    };
+
+    return getRes;
+  }, []);
   return (
     <Routes>
       <Route index element={<ShoppingComponent />} ></Route>
