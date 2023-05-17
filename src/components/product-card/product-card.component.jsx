8d9dc3cd505cae2f6652352db0da58e6bb06bd65
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2023-05-17 23:31:49 +0100

    css fix

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -93,7 +93,7 @@ const ProductCard = ({ products }) => {
             </span>
             <a
               href="#"
-              className="text-white bg-main hover:bg-black font-serrat focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[5px] md:text-sm px-1 md:px-5 py-2.5 text-center "
+              className="text-white bg-main hover:bg-black font-serrat focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[10px] lg:text-[5px] md:text-sm px-1 md:px-5 py-2.5 text-center "
               onClick={addToCartHandler}
             >
               Add to cart
